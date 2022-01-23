36313cc3a689b288ca4dfccc94241c3a23271d4b
nachtjasmin <nachtjasmin@outlook.de>
2022-01-23 17:51:54 +0100

    feat: write version to footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,21 @@
 <script>
 	import "../app.css";
+	import version from "../data/version.txt?raw";
 </script>
 
 <div class="container mx-auto my-4 md:mt-12 md:mb-8 lg:my-24 max-w-2xl px-4">
 	<slot />
-
-	<ul class="flex flex-col sm:flex-row space-x-4 justify-center">
-		<li><a href="/" class="footer-link">Zum Formular</a></li>
+	<ul class="flex flex-col sm:flex-row space-x-4 justify-center mb-8">
+		<li><a href="/" class="footer-link">Startseite</a></li>
 		<li><a href="/imprint" class="footer-link">Impressum & Datenschutzerklärung</a></li>
 	</ul>
+
+	<!-- The app version is set in the svelte.config.js -->
+	<p class="text-center italic  text-xs mt-8 text-gray-500">Version: {version}</p>
 </div>
 
 <style lang="postcss">
 	.footer-link {
-		@apply text-xs text-gray-500 underline;
+		@apply text-sm text-gray-500 underline;
 	}
 </style>

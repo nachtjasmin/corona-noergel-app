755cfd43c23f4f22ea114e16b1f825e12da0205e
nachtjasmin <nachtjasmin@outlook.de>
2022-01-23 16:41:24 +0100

    style: move outer container to layout, add mobile padding

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,6 @@
 	import "../app.css";
 </script>
 
-<slot />
+<div class="container mx-auto my-4 md:mt-12 md:mb-8 lg:my-24 max-w-2xl px-4">
+	<slot />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -84,86 +84,84 @@
 	};
 </script>
 
-<div class="container mx-auto my-4 md:mt-12 md:mb-8 lg:my-24 max-w-2xl">
-	<h1 class="font-bold text-2xl">Corona-Nörgel-App 🦠😷</h1>
-	<p class="mt-4">
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex facere animi quibusdam mollitia
-		doloremque, illo quis molestiae ipsa officiis sunt eius placeat dolorem a praesentium aliquid
-		corporis cum. Doloribus, distinctio?
-	</p>
-	<form on:submit|preventDefault={buildText}>
-		<section>
-			<p class="section-header">Schritt 1: Bundesland auswählen</p>
-			<select bind:value={empfaenger}>
-				<option disabled>Bundesland auswählen</option>
-				{#each Object.keys(config.bundeslaender) as land}
-					<option value={land}>
-						{config.bundeslaender[land].land}
-					</option>
-				{/each}
-			</select>
-		</section>
-		<section class:hidden={!showSecondStep}>
-			<div class="flex flex-col md:flex-row items-baseline justify-between">
-				<p class="section-header">Schritt 2: Textbausteine wählen</p>
-				<button type="button" class="btn-random" on:click={buildRandom}>Zufällig auswählen</button>
-			</div>
-			<label for="anrede">Anrede</label>
-			<select id="anrede" bind:value={data.anrede}>
-				<option disabled>Anrede auswählen</option>
-				{#each anreden as s}
-					<option value={s}>{s} </option>
-				{/each}
-			</select>
-
-			<label for="einleitung">Einleitung</label>
-			<select id="einleitung" bind:value={data.einleitung}>
-				<option disabled>Einleitung auswählen</option>
-				{#each config.einleitung as s}
-					<option value={s}>{s} </option>
-				{/each}
-			</select>
-
-			<label for="beschwerde">Beschwerde</label>
-			<select id="beschwerde" bind:value={data.beschwerde}>
-				<option disabled>Beschwerde auswählen</option>
-				{#each config.beschwerde as s}
-					<option value={s}>{s.text} </option>
-				{/each}
-			</select>
-
-			<label for="appell">Appell</label>
-			<select id="appell" bind:value={data.appell}>
-				<option disabled>Appell auswählen</option>
-				{#each appelle as s}
-					<option value={s}>{s.text} </option>
-				{/each}
-			</select>
-
-			<label for="gruss">Grußwort</label>
-			<select id="gruss" bind:value={data.gruss}>
-				<option disabled>Gruß auswählen</option>
-				{#each config.gruss as s}
-					<option value={s}>{s}</option>
-				{/each}
-			</select>
-		</section>
-		<section class:hidden={!showThirdStep}>
-			<p class="section-header">Schritt 3: Text erzeugen</p>
-			<button type="submit" class="btn">Bastel mir den Text!</button>
-			<textarea readonly class="w-full bg-gray-100 rounded mt-4" rows="10">{finalText}</textarea>
-		</section>
-	</form>
-	<section class:hidden={!showSendButton}>
-		<p class="section-header">Schritt 4: Mail verschicken</p>
-		<p class="text-sm">
-			Bei dem Klick auf den folgenden Button wird ein sogenannter <code>mailto:</code>-Link erzeugt.
-			Dieser öffnet dein E-Mail-Programm mit dem obigen Text. Dabei werden zu keinem Zeitpunkt
-			Informationen an uns übermittelt.
-		</p>
-		<a href={mailto} class="btn mt-4">Mail senden</a>
+<h1 class="font-bold text-2xl">Corona-Nörgel-App 🦠😷</h1>
+<p class="mt-4">
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex facere animi quibusdam mollitia
+	doloremque, illo quis molestiae ipsa officiis sunt eius placeat dolorem a praesentium aliquid
+	corporis cum. Doloribus, distinctio?
+</p>
+<form on:submit|preventDefault={buildText}>
+	<section>
+		<p class="section-header">Schritt 1: Bundesland auswählen</p>
+		<select bind:value={empfaenger}>
+			<option disabled>Bundesland auswählen</option>
+			{#each Object.keys(config.bundeslaender) as land}
+				<option value={land}>
+					{config.bundeslaender[land].land}
+				</option>
+			{/each}
+		</select>
 	</section>
-</div>
+	<section class:hidden={!showSecondStep}>
+		<div class="flex flex-col md:flex-row items-baseline justify-between">
+			<p class="section-header">Schritt 2: Textbausteine wählen</p>
+			<button type="button" class="btn-random" on:click={buildRandom}>Zufällig auswählen</button>
+		</div>
+		<label for="anrede">Anrede</label>
+		<select id="anrede" bind:value={data.anrede}>
+			<option disabled>Anrede auswählen</option>
+			{#each anreden as s}
+				<option value={s}>{s} </option>
+			{/each}
+		</select>
+
+		<label for="einleitung">Einleitung</label>
+		<select id="einleitung" bind:value={data.einleitung}>
+			<option disabled>Einleitung auswählen</option>
+			{#each config.einleitung as s}
+				<option value={s}>{s} </option>
+			{/each}
+		</select>
+
+		<label for="beschwerde">Beschwerde</label>
+		<select id="beschwerde" bind:value={data.beschwerde}>
+			<option disabled>Beschwerde auswählen</option>
+			{#each config.beschwerde as s}
+				<option value={s}>{s.text} </option>
+			{/each}
+		</select>
+
+		<label for="appell">Appell</label>
+		<select id="appell" bind:value={data.appell}>
+			<option disabled>Appell auswählen</option>
+			{#each appelle as s}
+				<option value={s}>{s.text} </option>
+			{/each}
+		</select>
+
+		<label for="gruss">Grußwort</label>
+		<select id="gruss" bind:value={data.gruss}>
+			<option disabled>Gruß auswählen</option>
+			{#each config.gruss as s}
+				<option value={s}>{s}</option>
+			{/each}
+		</select>
+	</section>
+	<section class:hidden={!showThirdStep}>
+		<p class="section-header">Schritt 3: Text erzeugen</p>
+		<button type="submit" class="btn">Bastel mir den Text!</button>
+		<textarea readonly class="w-full bg-gray-100 rounded mt-4" rows="10">{finalText}</textarea>
+	</section>
+</form>
+<section class:hidden={!showSendButton}>
+	<p class="section-header">Schritt 4: Mail verschicken</p>
+	<p class="text-sm">
+		Bei dem Klick auf den folgenden Button wird ein sogenannter <code>mailto:</code>-Link erzeugt.
+		Dieser öffnet dein E-Mail-Programm mit dem obigen Text. Dabei werden zu keinem Zeitpunkt
+		Informationen an uns übermittelt.
+	</p>
+	<a href={mailto} class="btn mt-4">Mail senden</a>
+</section>
 
 <style lang="postcss">
 	label {

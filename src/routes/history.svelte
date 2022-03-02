442be793db3fd5c9bd4a040022b9d26c69f692c2
nachtjasmin <nachtjasmin@outlook.de>
2022-03-02 18:40:59 +0100

    feat: add history page

---
+++ src/routes/history.svelte
@@ -0,0 +1,23 @@
+<h1 class="text-2xl font-bold">Historie von beschweren.rocks</h1>
+<div class="prose mt-4 dark:prose-invert">
+	<p>
+		Am 22.01. haben die Gesundheitsministieren in einer Telefonkonferenz weitgehende <a
+			href="https://www.gmkonline.de/Beschluesse.html?uid=268&jahr=2022"
+			>Einschränkungen für PCR-Tests</a
+		> beschlossen.
+	</p>
+	<p>
+		<b>Update (25.01.2022):</b> Wir haben die Kontaktdaten um die jeweiligen Staats- und Senatskanzleien
+		erweitert, sodass diese nun auch auf einfachem Wege kontaktiert werden können.
+	</p>
+	<p>
+		<b>Update (01.02.2022):</b> Es gibt nun deutlich mehr Beschwerden und Forderungen. Die sind nun auch
+		gruppiert, was im ersten Schritt dann ausgewählt werden kann. Dazu noch Fehlerbehebungen und Verbesserungen
+		in der UX und Barrierfreiheit.
+	</p>
+	<p>
+		<b>Update (08.02.2022):</b> Teilerfolg bei PCR-Tests.
+		<a href="https://twitter.com/MDRAktuell/status/1491006160874483713">Laut MDR</a>
+		kündigt Lauterbach an, dass Anspruch auf PCR-Tests bei positivem Schnelltest weiterhin bestehen soll.
+	</p>
+</div>

f1fcd88fc2459201a874eca651d309148ae361ba
nachtjasmin <nachtjasmin@outlook.de>
2022-07-17 10:54:06 +0200

    add faq

---
+++ src/routes/faq.svelte
@@ -0,0 +1,127 @@
+<h1 class="text-2xl font-bold">Fragen und Antworten zu Beschweren.Rocks</h1>
+<div class="prose mt-4 mb-8 dark:prose-invert">
+	<h2 class="sr-only">Allgemeine Fragen</h2>
+	<h3>Was ist das hier?</h3>
+	<p>
+		Hier gibt es Vorschläge für Formulierungen und Kontaktinformationen für pandemiepolitische
+		Appelle. In wenigen Klicks gibt es einen Vorschlag für ein Schreiben und die Kontaktdaten, um
+		das erzeugte Schreiben per Brief, Fax oder per Mail zu verschicken.
+	</p>
+	<h3>Was soll das?</h3>
+	<p>
+		Wir sind der Ansicht, dass es auf gesellschaftlicher und individueller Ebene wichtig und
+		deutlich besser als der Status Quo wäre, wenn es deutlichere und stärkere Leitlinien gegen diese
+		Pandemie aus der Politik gäbe.<br />
+		Gleichzeitig ist es oft schwierig, eine Beschwerde, eine Bitte, eine Rückmeldung oder eine Forderung
+		an <em>die Politik</em> zu formulieren. Hier liefern wir Vorschläge.
+	</p>
+	<h3>Wer ist wir? Wer steht hinter Beschweren.Rocks?</h3>
+	<p>
+		<a href="https://github.com/xenein" rel="noopener">Xenia</a> und
+		<a href="https://github.com/chloe-the-catgirl" rel="noopener">Jasmin</a> entwickeln die Software,
+		die Beschweren.Rocks betreibt, setzen Themen und stellen die Formulierungsvorschläge zusammen. Beide
+		entwerfen und erstellen hauptberuflich Software. [hier gegebenenfalls noch 1,5 biographische Sätze,
+		falls wir wollen sollten]
+	</p>
+	<h3>Was mache ich, wenn ich einen Fehler auf Beschweren.Rocks gefunden habe?</h3>
+	<p>
+		Gerne unter <a href="mailto:hallo@beschweren.rocks">hallo@beschweren.rocks</a> Bescheid geben.
+		Falls es sich um einen Programmierfehler handelt, gerne auch eine
+		<a href="https://github.com/chloe-the-catgirl/corona-noergel-app" rel="noopener"
+			>Issue auf GitHub</a
+		> öffnen.
+	</p>
+	<h3>Wieso gibt es bei den Empfänger*innen keine Einträge für Personen bei der AfD?</h3>
+	<p>
+		<a href="https://youtu.be/mFML30AQuT8?t=201" rel="noopener"
+			>Das hat Danger Dan schon mal sehr deutlich auf den Punkt gebracht</a
+		>:
+	</p>
+	<blockquote>
+		<p>
+			Faschisten hören niemals auf Faschisten zu sein<br />
+			Man diskutiert mit ihnen nicht, das hat die Geschichte gezeigt<br />
+			— <cite>Danger Dan</cite>
+		</p>
+	</blockquote>
+	<h3>Könnt ihr Beschweren.Rocks auch für ein anderes Anliegen bereitsstellen?</h3>
+	<p>
+		Theoretisch ja. Praktisch haben wir vermutlich keine Zeit dazu, da wir Beschweren.Rocks in
+		unserer Freizeit vorantreiben. Mit vielen Themen kennen wir uns auch nicht so gut aus und sind
+		nicht tief genug drin, dass wir ähnlich solide Vorschläge wie für Beschweren.Rocks liefern
+		könnten.
+	</p>
+	<p>
+		Falls du trotzdem glaubst, dass du ein Anliegen hast, das von einer Seite basierend auf der
+		Software in Beschweren.Rocks profitieren würde, hast du im Wesentlichen zwei Möglichkeiten:
+	</p>
+	<ul>
+		<li>
+			Eine Mail an <a href="mailto:hallo@beschweren.rocks">hallo@beschweren.rocks</a> schreiben und nachfragen,
+			ob wir dazu was machen können und werden. Das ist in den meisten Fällen aber sehr unwahrscheinlich
+		</li>
+		<li>
+			Unser Programmcode ist öffentlich auf <a
+				href="https://github.com/chloe-the-catgirl/corona-noergel-app"
+				rel="noopener">GitHub</a
+			>. Die Lizenz ist frei und das Tool kann geforked und für den eigenen Bedarf angepasst werden.
+			Das Frontend basiert auf Svelte mit TypeScript und TailwindCSS. Die Inhalte werden aus
+			JSON-Dateien generiert.
+		</li>
+	</ul>
+	<h3>Wieso richtet ihr euch an deutsche Politik, habt aber ein Impressum in Österreich?</h3>
+	<p>
+		Rechtlich müssen wir eines vorhalten. Wir wissen aber sehr genau, dass es Menschen gibt, die
+		sehr gegenteilige Ansichten über die Notwendigkeiten von Maßnahmen zur Eindämmung dieser
+		Pandemie haben, und dass sich unter diesen auch einige befinden, bei denen wir gerne vermeiden
+		würden, dass sie sich bei uns zum Kaffee einladen. Der Impressumskontakt erfüllt die rechtlichen
+		Vorgaben für ein Impressum - und schützt uns dabei weitestgehend.
+	</p>
+	<h2>Ich habe Post von euch bekommen / Ich bin Politiker*in</h2>
+	<h3>Kann ich Mails von Beschweren.Rocks deaktivieren?</h3>
+	<p>
+		Das wird schwierig, Beschweren.Rocks verschickt keine Mails. Wir liefern nur
+		Formulierungsvorschläge. Mit dem Versand selbst haben wir nichts zu tun.
+	</p>
+	<h3>Gibt es die Positionen von Beschweren.Rocks irgendwo gesammelt?</h3>
+	<p>
+		Wir haben <a href="/unsere-position">unsere Position</a> aufgeschrieben.
+	</p>
+	<h3>Von was für einer Organisation geht Beschweren.Rocks aus?</h3>
+	<p>
+		Keiner. <a href="https://github.com/xenein" rel="noopener">Xenia</a> und
+		<a href="https://github.com/chloe-the-catgirl" rel="noopener">Jasmin</a> haben die Inhalte zusammengestellt.
+		Es gab und gibt auch keine finanziellen oder materiellen Zuwendungen.
+	</p>
+	<h3>Steht ihr für Hintergrundgespräche, Stellungnahmen oder Anhörungen zur Verfügung?</h3>
+	<p>
+		Wir sind nur Programmierer*innen, die mal eine Hochschule besucht haben und wissenschaftliche
+		Arbeitsweise und Umgang mit wissenschaftlicher Erkenntnis ein wenig beherrschen. Es sollte zu
+		allen Themen, die wir ansprechen, Menschen mit breiterer Expertise geben.
+	</p>
+	<p>
+		Falls es dennoch Anfragen gibt, nehmen wir diese, so sie aus den demokratischen Fraktionen
+		kommen, gerne unter <a href="mailto:hallo@beschweren.rocks">hallo@beschweren.rocks</a> entgegen.
+	</p>
+	<h2>Ich arbeite für eine Redaktion</h2>
+	<h3>Gebt ihr Interviews zu Beschweren.Rocks?</h3>
+	<p>
+		Wir programmieren und formulieren für Beschweren.Rocks in unserer Freizeit, ehrenamtlich und
+		unbezahlt. Daher können wir - insbesondere für kurzfristige Anfragen - keine rechtzeitigen
+		Antworten garantieren.
+	</p>
+	<p>
+		Anfragen, insofern das Medium, in dem veröffentlicht werden soll, nicht dem Springer-Verlag
+		angeschlossen ist, gerne an <a href="mailto:presse@beschweren.rocks">presse@beschweren.rocks</a
+		>.
+	</p>
+	<h3>
+		Wie viele Aufrufe auf Beschweren.Rocks gibt es? Wie viele Mails wurden bereits verschickt?
+	</h3>
+	<p>
+		Das wissen wir nicht. Wir haben strenge Datensparsamkeit über unsere Neugierde gestellt. Rein
+		technisch könnten wir nichts außer die reinen Seitenaufrufe zählen, worauf wir allerdings
+		ebenfalls verzichten. Alles, was im Browser passiert, bleibt im Browser. Es werden keine Daten
+		zu uns zurückgeschickt, die wir dazu auswerten könnten.
+	</p>
+</div>
